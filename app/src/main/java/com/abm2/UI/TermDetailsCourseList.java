b79ce366d750e2668e3a5a02509bd14d181c0b36
Seth Gustafson <sethrow1995@gmail.com>
2022-04-01 11:43:51 -0700

    Added term details and course list

--- app/src/main/java/com/abm2/UI/TermDetailsCourseList.java
+++ app/src/main/java/com/abm2/UI/TermDetailsCourseList.java
@@ -2,15 +2,73 @@ package com.abm2.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.abm2.R;
 
+import java.util.Calendar;
+
 public class TermDetailsCourseList extends AppCompatActivity {
+
+    private EditText startDateText;
+    private EditText endDateText;
+
+    private int date, month, year;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_details_course_list);
+
+        startDateText = findViewById(R.id.startDateText);
+        endDateText = findViewById(R.id.endDateText);
+    }
+
+    public void onStartDateImgClick(View view) {
+        final Calendar cal = Calendar.getInstance();
+        date = cal.get(Calendar.DATE);
+        month = cal.get(Calendar.MONTH);
+        year = cal.get(Calendar.YEAR);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(TermDetailsCourseList.this, android.R.style.Theme_Holo_Light_Dialog,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+                        month += 1;
+                        startDateText.setText(month+"-"+date+"-"+year);
+                    }
+                }, year, date, month);
+        datePickerDialog.show();
+    }
+
+    public void onEndDateImgClick(View view) {
+        final Calendar cal = Calendar.getInstance();
+        date = cal.get(Calendar.DATE);
+        month = cal.get(Calendar.MONTH);
+        year = cal.get(Calendar.YEAR);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(TermDetailsCourseList.this, android.R.style.Theme_Holo_Light_Dialog,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+                        month += 1;
+                        endDateText.setText(month+"-"+date+"-"+year);
+                    }
+                }, year, date, month);
+        datePickerDialog.show();
+    }
+
+    public void onAddBtnClick(View view) {
+
+    }
+
+    public void onTextCourseTitleClick(View view) {
+        Intent intent = new Intent(TermDetailsCourseList.this, CourseDetailsAssessmentList.class);
+        startActivity(intent);
     }
 }
