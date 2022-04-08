52945b5eac7bfb636f4025376a749efce42bd2cc
Seth Gustafson <sethrow1995@gmail.com>
2022-04-07 18:58:25 -0700

    Added ability to add new terms

--- app/src/main/java/com/abm2/UI/TermList.java
+++ app/src/main/java/com/abm2/UI/TermList.java
@@ -7,41 +7,131 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.abm2.Database.Repository;
+import com.abm2.Entity.Assessment;
 import com.abm2.Entity.Term;
 import com.abm2.R;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 public class TermList extends AppCompatActivity {
-
-    private EditText startDateText;
-    private EditText endDateText;
+    //Set fields for layout items
+    private EditText editNewTermTitle;
+    private EditText editNewTermStartDate;
+    private EditText editNewTermEndDate;
 
     //Prepare date fields
     final Calendar CAL = Calendar.getInstance();
     private int date, month, year;
-    Date startDate = new Date();
-    Date endDate = new Date();
+
+    //Prepare fields for new Term creation
+    private String newTitle;
+    private Date newStartDate = new Date();
+    private Date newEndDate = new Date();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_list);
 
-        startDateText = findViewById(R.id.editStartDate);
-        endDateText = findViewById(R.id.editEndDate);
+        //Set layout items
+        editNewTermTitle = findViewById(R.id.editNewTermTitle);
+        editNewTermStartDate = findViewById(R.id.editNewTermStartDate);
+        editNewTermEndDate = findViewById(R.id.editNewTermEndDate);
 
+        //Populate recycler view with Term items from DB
+        refreshRecycler();
+    }
+
+    public void onStartDateClick(View view) {
+        date = CAL.get(Calendar.DATE);
+        month = CAL.get(Calendar.MONTH);
+        year = CAL.get(Calendar.YEAR);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(TermList.this, android.R.style.Theme_Holo_Light_Dialog,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+                        editNewTermStartDate.setText((month+1)+"-"+date+"-"+year);
+                        CAL.set(year, month, date);
+                        newStartDate.setTime(CAL.getTimeInMillis());
+                    }
+                }, year, month, date);
+        datePickerDialog.show();
+    }
+
+    public void onEndDateClick(View view) {
+        date = CAL.get(Calendar.DATE);
+        month = CAL.get(Calendar.MONTH);
+        year = CAL.get(Calendar.YEAR);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(TermList.this, android.R.style.Theme_Holo_Light_Dialog,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+                        editNewTermEndDate.setText((month+1)+"-"+date+"-"+year);
+                        CAL.set(year, month, date);
+                        newEndDate.setTime(CAL.getTimeInMillis());
+                    }
+                }, year, month, date);
+        datePickerDialog.show();
+    }
+
+    public void onBtnAddTermClick(View view) {
+        Repository repo = new Repository(getApplication());
+        //Get new ID for term by getting last term and incrementing ID
+        List<Term> allTerms = repo.selectAllTerms();
+        int newId = (allTerms.get(allTerms.size()-1).getTermId()) + 1;
+        newTitle = null;
+        try {
+            newTitle = String.valueOf(editNewTermTitle.getText());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        //Check for blank fields
+        if (newTitle.equals("") || newTitle.equals(null)) {
+            //Set Toast error message then show to user
+            Toast toast = Toast.makeText(getApplication(), "Title field must not be blank", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER | Gravity.CENTER_HORIZONTAL, 0, 0);
+            toast.show();
+        }
+        else {
+            if (newEndDate.before(newStartDate) || newEndDate.equals(newStartDate)) {
+                //Set Toast error message then show to user
+                Toast toast = Toast.makeText(getApplication(), "Term end date must be after term start date", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER | Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.show();
+            }
+            else {
+                Term newTerm = new Term(newId, newTitle, newStartDate, newEndDate);
+                repo.insert(newTerm);
+
+                //Set Toast error message then show to user
+                Toast toast = Toast.makeText(getApplication(), "Term saved!", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER | Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.show();
+            }
+        }
+        refreshRecycler();
+    }
+
+    public void onTermTitleTextClick(View view) {
+        Intent intent = new Intent(TermList.this, TermDetailsCourseList.class);
+        startActivity(intent);
+    }
+
+    public void refreshRecycler() {
         //Populate recycler view with Term items from DB
         RecyclerView recyclerView = findViewById(R.id.rvTerms);
         Repository repo = new Repository(getApplication());
@@ -50,62 +140,5 @@ public class TermList extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter.setTerms(allTerms);
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        //Adds items to menu if present
-        getMenuInflater().inflate(R.menu.menu_termlist, menu);
-        return true;
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            this.finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void onStartDateImgClick(View view) {
-        date = CAL.get(Calendar.DATE);
-        month = CAL.get(Calendar.MONTH);
-        year = CAL.get(Calendar.YEAR);
-
-        DatePickerDialog datePickerDialog = new DatePickerDialog(TermList.this, android.R.style.Theme_Holo_Light_Dialog,
-                new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker datePicker, int year, int month, int date) {
-                        startDateText.setText((month+1)+"-"+date+"-"+year);
-                        CAL.set(year, month, date);
-                    }
-                }, year, month, date);
-        datePickerDialog.show();
-    }
-
-    public void onEndDateImgClick(View view) {
-        date = CAL.get(Calendar.DATE);
-        month = CAL.get(Calendar.MONTH);
-        year = CAL.get(Calendar.YEAR);
-
-        DatePickerDialog datePickerDialog = new DatePickerDialog(TermList.this, android.R.style.Theme_Holo_Light_Dialog,
-                new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker datePicker, int year, int month, int date) {
-                        endDateText.setText((month+1)+"-"+date+"-"+year);
-                        CAL.set(year, month, date);
-                    }
-                }, year, month, date);
-        //TODO set minimum for end date to startdate
-//        datePickerDialog.getDatePicker().setMinDate(whatever start date is);
-        datePickerDialog.show();
-    }
-
-    public void onBtnAddTermClick(View view) {
-
-    }
-
-    public void onTermTitleTextClick(View view) {
-        Intent intent = new Intent(TermList.this, TermDetailsCourseList.class);
-        startActivity(intent);
     }
 }
