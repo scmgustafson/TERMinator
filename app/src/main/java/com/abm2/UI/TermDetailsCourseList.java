07bb82640eee7df2d5beaca668ecf704e0c0452f
Seth Gustafson <sethrow1995@gmail.com>
2022-04-09 11:13:20 -0700

    Added ability to add new Courses with error/exception handling

--- app/src/main/java/com/abm2/UI/TermDetailsCourseList.java
+++ app/src/main/java/com/abm2/UI/TermDetailsCourseList.java
@@ -54,8 +54,8 @@ public class TermDetailsCourseList extends AppCompatActivity {
     //Declare fields for new course object creation
     private int newCourseId;
     private String newCourseTitle;
-    private Date newCourseStartDate;
-    private Date newCourseEndDate;
+    private Date newCourseStartDate = new Date();
+    private Date newCourseEndDate = new Date();
     private String newCourseStatus;
     private String newCourseIName;
     private String newCourseIPhone;
@@ -101,12 +101,16 @@ public class TermDetailsCourseList extends AppCompatActivity {
         month = SENT_INFO_CAL.get(Calendar.MONTH);
         year = SENT_INFO_CAL.get(Calendar.YEAR);
         editTermEndDate.setText((month+1)+"-"+date+"-"+year);
-
-        //Set new Course layout items
-        editNewCourseStartDate = findViewById(R.id.editNewCourseStartDate);
-        editNewCourseEndDate = findViewById(R.id.editNewCourseEndDate);
         newTermStartDate = sentStartDate;
         newTermEndDate = sentEndDate;
+
+        //Set new Course layout items
+        editNewCourseTitle = findViewById(R.id.editNewCourseTitle);
+        editNewCourseStartDate = findViewById(R.id.editNewCourseStartDate);
+        editNewCourseEndDate = findViewById(R.id.editNewCourseEndDate);
+        editNewCourseIName = findViewById(R.id.editNewCourseIName);
+        editNewCourseIPhone = findViewById(R.id.editNewCourseIPhone);
+        editNewCourseIEmail = findViewById(R.id.editNewCourseIEmail);
 
         //Set spinner information and populate
         ArrayAdapter<CharSequence> statusAdapter = ArrayAdapter.createFromResource(this, R.array.status_array, android.R.layout.simple_spinner_item);
@@ -114,7 +118,8 @@ public class TermDetailsCourseList extends AppCompatActivity {
         statusAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         statusSpinner.setAdapter(statusAdapter);
 
-        refreshRecycler();
+        //Refresh Course RV
+        refreshCourseRecyclerView();
     }
 
     public void onTermStartDateClick(View view) {
@@ -195,6 +200,7 @@ public class TermDetailsCourseList extends AppCompatActivity {
     public void onBtnDeleteTermClick(View view) {
         repo = new Repository(getApplication());
         List<Course> currentTermCourses = repo.selectCourseByTerm(sentTerm.getTermId());
+        //Check to see if a Term has existing courses, if so then display error message otherwise delete the term
         if (currentTermCourses.size() > 0) {
             Toast toast = Toast.makeText(getApplication(), "Term cannot be deleted because it has existing courses. Please delete all courses for this term first.", Toast.LENGTH_LONG);
             toast.setGravity(Gravity.CENTER | Gravity.CENTER_HORIZONTAL, 0, 0);
@@ -224,6 +230,7 @@ public class TermDetailsCourseList extends AppCompatActivity {
                     public void onDateSet(DatePicker datePicker, int year, int month, int date) {
                         editNewCourseStartDate.setText((month+1)+"-"+date+"-"+year);
                         NEW_COURSE_CAL.set(year, month, date);
+                        newCourseStartDate.setTime(NEW_COURSE_CAL.getTimeInMillis());
                     }
                 }, year, month, date);
         datePickerDialog.show();
@@ -240,14 +247,64 @@ public class TermDetailsCourseList extends AppCompatActivity {
                     public void onDateSet(DatePicker datePicker, int year, int month, int date) {
                         editNewCourseEndDate.setText((month+1)+"-"+date+"-"+year);
                         NEW_COURSE_CAL.set(year, month, date);
+                        newCourseEndDate.setTime(NEW_COURSE_CAL.getTimeInMillis());
                     }
                 }, year, month, date);
         datePickerDialog.show();
     }
 
     public void onBtnAddNewCourseClick(View view) {
+        Repository repo = new Repository(getApplication());
+        //Get new ID for Course by getting last existing course and incrementing ID or starting at 1
+        newCourseId = 1;
+        List<Course> allCourses = repo.selectAllCourses();
+        if (allCourses.size() > 0) {
+            newCourseId = (allCourses.get(allCourses.size()-1).getTermId()) + 1;
+        }
+        newCourseTitle = null;
+        newCourseIName = null;
+        newCourseIPhone = null;
+        newCourseIEmail = null;
+        //Read in values of EditText fields for new object creation
+        try {
+            newCourseTitle = String.valueOf(editNewCourseTitle.getText());
+            newCourseStatus = statusSpinner.getSelectedItem().toString();
+            newCourseIName = String.valueOf(editNewCourseIName.getText());
+            newCourseIPhone = String.valueOf(editNewCourseIPhone.getText());
+            newCourseIEmail = String.valueOf(editNewCourseIEmail.getText());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        //Check for blank fields
+        if ((newCourseTitle.equals("") || newCourseTitle.equals(null)) || (newCourseIName.equals("") || newCourseIName.equals(null))
+        || (newCourseIPhone.equals("") || newCourseIPhone.equals(null)) || (newCourseIEmail.equals("") || newCourseIEmail.equals(null))) {
+            //Set Toast error message then show to user
+            Toast toast = Toast.makeText(getApplication(), "Title field must not be blank", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER | Gravity.CENTER_HORIZONTAL, 0, 0);
+            toast.show();
+        }
+        else {
+            //Check dates
+            if (newCourseEndDate.before(newCourseStartDate) || newCourseEndDate.equals(newCourseStartDate) ||
+                    ((sentTerm.getStartDate().after(newCourseStartDate)) || (sentTerm.getStartDate().after(newCourseEndDate))) ||
+                    ((sentTerm.getEndDate().before(newCourseStartDate)) || (sentTerm.getEndDate().before(newCourseEndDate)))) {
+                //Set Toast error message then show to user
+                Toast toast = Toast.makeText(getApplication(), "Course end date must be after course start date and must be within term dates", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER | Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.show();
+            }
+            else {
+                Course newCourse = new Course(newCourseId, newCourseTitle, newCourseStartDate, newCourseEndDate, newCourseStatus, newCourseIName, newCourseIPhone, newCourseIEmail, sentTerm.getTermId());
+                repo.insert(newCourse);
 
-        refreshRecycler();
+                //Set Toast error message then show to user
+                Toast toast = Toast.makeText(getApplication(), "Course saved!", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER | Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.show();
+            }
+        }
+        refreshCourseRecyclerView();
     }
 
     public void onTextCourseTitleClick(View view) {
@@ -255,8 +312,8 @@ public class TermDetailsCourseList extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void refreshRecycler() {
-        //Populate recycler view with Term items from DB
+    public void refreshCourseRecyclerView() {
+        //Populate recycler view with Course items from DB
         RecyclerView recyclerView = findViewById(R.id.rvCourses);
         Repository repo = new Repository(getApplication());
         List<Course> allCourses = repo.selectCourseByTerm(sentId);
